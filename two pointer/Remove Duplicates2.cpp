ec5a81734ea1e816336522ce075e23443b48660b
Tim Harasaki <harasakitim0418@gmail.com>
2026-04-03 23:59:20 +0800

    Update Remove Duplicates2.cpp

--- two pointer/Remove Duplicates2.cpp
+++ two pointer/Remove Duplicates2.cpp
@@ -4,7 +4,7 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         // 一樣用雙指標
-        int slow=2;
+        int slow=1;
         for(int fast=2;fast<nums.size();fast++){
             if(nums[fast]!=nums[fast-2]){
                 nums[slow]=nums[fast];
