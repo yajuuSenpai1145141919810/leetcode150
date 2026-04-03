b6fa41291feaba93ff61dc7a44b9b89675281fc1
Tim Harasaki <harasakitim0418@gmail.com>
2026-04-03 23:28:21 +0800

    Create Remove Duplicates.cpp

---
+++ two pointer/Remove Duplicates.cpp
@@ -0,0 +1,19 @@
+// 26. Remove Duplicates from Sorted Array
+// 對照27題
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        // 很難說明 但就是 如果要往後找我要的東西放到前面 用雙指標
+
+        int slow=0;
+        for(int fast=1;fast<nums.size();fast++){
+            if(nums[fast]>nums[slow]){
+                slow++;
+                nums[slow]=nums[fast];
+            }
+        }
+        // [1,2,3] 一開始slow是在0喔,兩輪做完後 slow=2(位子2),但共有三個元素所以要+1
+        return slow+1;        
+    }
+};
+
