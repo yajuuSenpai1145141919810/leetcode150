89f4abb4dbc02b69618be798388c74254557d18a
Tim Harasaki <harasakitim0418@gmail.com>
2026-04-03 23:54:35 +0800

    Update Remove Duplicates2.cpp
    還有一些錯誤 等等改 好累

--- two pointer/Remove Duplicates2.cpp
+++ two pointer/Remove Duplicates2.cpp
@@ -1,1 +1,16 @@
 // 80. Remove Duplicates from Sorted Array II
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        // 一樣用雙指標
+        int slow=2;
+        for(int fast=2;fast<nums.size();fast++){
+            if(nums[fast]!=nums[fast-2]){
+                nums[slow]=nums[fast];
+                slow++;
+            }
+        }        
+        return slow;
+    }
+};
