8d26de3e9fdb0f86722db4c3f6e624d6cb2f4823
Tim Harasaki <harasakitim0418@gmail.com>
2026-04-04 23:33:52 +0800

    Update LRU Cache.cpp
    好難

--- LRU Cache.cpp
+++ LRU Cache.cpp
@@ -7,50 +7,50 @@
 
 class LRUCache {
 private:
-    int cap;
-    // list 儲存 {key, value}，最近使用的放在 front
-    std::list<std::pair<int, int>> cacheList;
-    // map 儲存 key 到 list 迭代器 (iterator) 的映射，方便 O(1) 找到節點
-    std::unordered_map<int, std::list<std::pair<int, int>>::iterator> map;
+    //「Map (快速尋找) + List (維護順序) + Iterator (連結兩者的橋樑)」
+
+    int cap;// 這個 Cache 最大能放幾本書
+
+    // 一個雙向鏈結串列，裡面裝的每一個節點，都是一組 {Key, Value} 的組合
+    list<pair<int,int>> cachelist;
+
+    unordered_map<int,list<pair<int,int>>::iterator> map;
 
 public:
     LRUCache(int capacity) {
-        cap = capacity;
+        cap=capacity;        
     }
-    
+
+    // 找書(key)把裡面的內容value回傳
     int get(int key) {
-        // 如果 key 不存在，回傳 -1
-        if (map.find(key) == map.end()) {
-            return -1;
-        }
-        
-        // 如果存在，把該節點搬到 list 的最前面 (代表最近使用)
-        // splice(目標位置, 來源 list, 來源迭代器)
-        cacheList.splice(cacheList.begin(), cacheList, map[key]);
-        
+
+        if(map.find(key)==map.end()) return -1;
+
+        // splice 的語法：(搬去哪, 從哪個 list 搬, 搬哪一個節點)
+        cachelist.splice(cachelist.begin(),cachelist,map[key]);
+
+        // 回傳書裡面的內容
         return map[key]->second;
+
     }
-    
+    // put 就是「更新並搬到起點」或「淘汰終點並插入起點」。
     void put(int key, int value) {
-        // 1. 如果 key 已經存在
-        if (map.find(key) != map.end()) {
-            // 更新數值
-            map[key]->second = value;
-            // 搬到最前面
-            cacheList.splice(cacheList.begin(), cacheList, map[key]);
-            return;
-        }
+    // 情況 A：這本書已經在書架上 (Update)
+    // 更新書的內容（Value）。
+    // 因為剛動過它，所以把它搬到書架最前面。
+
+    // 情況 B：這是一本新書 (Insert)
+    // 檢查書架滿了沒？ 如果滿了，把書架「最後面」那本最久沒翻的書丟掉，同時把目錄（Map）裡的紀錄也刪掉。
+    // 把新書放最前面。
+    // 在目錄（Map）記下這本書的位置。
+
         
-        // 2. 如果 key 不存在，檢查是否滿了
-        if (cacheList.size() == cap) {
-            // 刪除最久沒用的 (list 最後面那個)
-            int lastKey = cacheList.back().first;
-            cacheList.pop_back();
-            map.erase(lastKey);
-        }
-        
-        // 3. 插入新節點到最前面
-        cacheList.push_front({key, value});
-        map[key] = cacheList.begin();
     }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
