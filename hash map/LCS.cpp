c5e1cbeb9dc57cfcd889dc057d475a55964b3f25
Tim Harasaki <harasakitim0418@gmail.com>
2026-04-06 23:48:58 +0800

    Create LCS.cpp

---
+++ hash map/LCS.cpp
@@ -0,0 +1,7 @@
+// 128. Longest Consecutive Sequence
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+// You must write an algorithm that runs in O(n) time.
+
+// Input: nums = [1,0,1,2]
+// Output: 3
+
